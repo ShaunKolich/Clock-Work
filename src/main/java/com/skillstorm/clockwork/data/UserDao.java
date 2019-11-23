f5b340132ff516055cf2c4603ddb7e3cf9dea1e4
ShaunKolich <employmeinc@gmail.com>
2019-11-22 23:12:12 -0500

    Fixed a error

--- src/main/java/com/skillstorm/clockwork/data/UserDao.java
+++ src/main/java/com/skillstorm/clockwork/data/UserDao.java
@@ -10,8 +10,6 @@ import java.util.Set;
 
 import javax.management.RuntimeErrorException;
 
-import org.chinook.beans.Artist;
-
 import com.skillstorm.clockwork.beans.Employee;
 
 public class UserDao {
