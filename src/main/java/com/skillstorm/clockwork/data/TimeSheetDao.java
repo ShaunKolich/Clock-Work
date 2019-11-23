1cc0edb07da58100599ad002f57aa5250b9ae5c2
ShaunKolich <employmeinc@gmail.com>
2019-11-23 16:51:47 -0500

    working on TimesheetDao

--- src/main/java/com/skillstorm/clockwork/data/TimeSheetDao.java
+++ src/main/java/com/skillstorm/clockwork/data/TimeSheetDao.java
@@ -25,47 +25,58 @@ public class TimeSheetDao {
 
 	}
 
-public Set<TimeSheet> searchByEmployee (String UserName){
-	Connection conn = getConnection();	
-	Set<TimeSheet> results = new HashSet<>();
-try {
-	PreparedStatement stmt = conn.prepareStatement("select * from Hours inner join employee on employee.User_Id = hours.User_Id where employee.UserName like ?");
-	stmt.setString(14, UserName + "%");
-	ResultSet result = stmt.executeQuery();
-		
-	while (result.next()) {
-			TimeSheet employee = new TimeSheet(result.getInt(1),result.getInt(2),result.getDouble(3));
-			TimeSheet Employee = new TimeSheet(result.getDouble(4),result.getDouble(5),result.getDouble(6),result.getDouble(7),result.getDouble(8));
-			results.add(employee);
-			results.add(Employee);
+	public Set<TimeSheet> searchByEmployee(String UserName) {
+		Connection conn = getConnection();
+		Set<TimeSheet> results = new HashSet<>();
+		try {
+			PreparedStatement stmt = conn.prepareStatement(
+					"select * from Hours inner join employee on employee.User_Id = hours.User_Id where employee.UserName like ?");
+			stmt.setString(14, UserName + "%");
+			ResultSet result = stmt.executeQuery();
+
+			while (result.next()) {
+				TimeSheet employee = new TimeSheet(result.getInt(1), result.getInt(2), result.getDouble(3));
+				TimeSheet Employee = new TimeSheet(result.getDouble(4), result.getDouble(5), result.getDouble(6),
+						result.getDouble(7), result.getDouble(8));
+				results.add(employee);
+				results.add(Employee);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
 		}
-} catch (SQLException e) {
-	throw new RuntimeException(e);
-} finally {
-	try {
-		conn.close();
-	} catch (SQLException e) {
-		throw new RuntimeException(e);
+		return results;
 	}
+
+	public void update(TimeSheet timesheet) {
+		Connection conn = getConnection();
+		String sql = "update hours h inner join employee e on e.User_Id = h.User_Id Set Mon_Hours = ?, Tues_Hours = ?, Wed_Hours = ?, Thurs_Hours = ?, Fri_Hours = ? where e.UserName like ?";
+		try {
+			conn.setAutoCommit(false);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setDouble(1, timesheet.getMon_Hours());
+			stmt.setDouble(2, timesheet.getTues_Hours());
+			stmt.setDouble(3, timesheet.getWeds_Hours());
+			stmt.setDouble(4, timesheet.getThurs_Hours());
+			stmt.setDouble(5, timesheet.getFri_Hours());
+			stmt.executeUpdate();
+			conn.commit();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
+
 }
-return results;
-}
-
-
-//public TimeSheet update(String UserName) {
-//	Connection conn = getConnection();
-//	String sql = "update hours h inner join employee e on e.User_Id = h.User_Id Set Mon_Hours = ?, Tues_Hours = ?, Wed_Hours = ?, Thurs_Hours = ?, Fri_Hours = ? where e.UserName like ?";
-//	try {
-//		conn.setAutoCommit(false);
-//		PreparedStatement stmt = conn.prepareStatement(sql);
-////		stmt.setDouble(3,TimeSheet.setMon_Hours());
-////		stmt.setDouble(4, TimeSheet.setTues_Mon());
-//		stmt.executeUpdate();
-//		conn.commit();
-//		
-//	}
-
-}
-
-
-
